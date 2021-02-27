ac4c38c7c62a09d220a0c51467448597c61dd579
Jimeneab <abraham.tal.ji@gmail.com>
2021-02-26 23:02:07 -0600

    search field

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import NavBar from './components/navBar/navBar'
+import TextField from '@material-ui/core/TextField';
+import SearchField from './components/searchField/index'
+
 
 function App() {
   return (
     <>
       <NavBar/>
+      <SearchField/>
     </>
   );
 }
