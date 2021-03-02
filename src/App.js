7355929fb90dc7a240b8eddc80447260f6d33963
Jimeneab <abraham.tal.ji@gmail.com>
2021-03-02 12:55:10 -0600

    first version with grid and navbar

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ import SearchField from './components/searchField/index'
 
 function App() {
   return (
-    <>
-      <NavBar/>
-      <SearchField/>
-    </>
+    <Grid container>
+      <Grid lg={12} md={12} xs={12} spacing={1}><NavBar/></Grid>
+      <Grid lg={4} md={6} xs={12} spacing={1}><Box border={1} bgcolor="primary.main" color="white">asdmaslmdl</Box></Grid>
+      <Grid lg={4} md={6} xs={12} spacing={1}><Box border={1} bgcolor="primary.main" color="white">asdmalsmd</Box></Grid>
+      <Grid lg={4} md={6} xs={12} spacing={1}><Box border={1} bgcolor="primary.main" color="white">sakkdnaklsn</Box></Grid>
+    </Grid>
   );
 }
 
